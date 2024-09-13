ff1d26316e8902727740fc5d49b7e5bb59e45567
Anil Ravilla <131127146+rakn7032@users.noreply.github.com>
2024-09-13 10:21:50 +0530

    Update admin.py

--- online_bookstore/books/admin.py
+++ online_bookstore/books/admin.py
@@ -3,7 +3,7 @@ from . import models
  
 class BookAdmin(admin.ModelAdmin):
     list_display=("id", "title","author", "published_date", "price")
-    fields = ("created_by", "title","author","description", "published_date", "price", "created_at", "updated_at")
+    fields = ("title","author","description", "published_date", "price", "created_at", "updated_at")
     readonly_fields=("created_at","updated_at")
  
 admin.site.register(models.Book, BookAdmin)
